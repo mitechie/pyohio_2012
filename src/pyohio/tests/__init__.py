096af4656f027e790d2ef8303d2966b93b3307c6
Richard Harding <rharding@mitechie.com>
2012-07-21 18:11:53 -0400

    Add test files

---
+++ src/pyohio/tests/__init__.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+import pyohio
+
+class TestHello(TestCase):
+    """Hello is such a powerful tool!"""
+
+    def test_hello_sucess(self):
+        """The power of hello is with you."""
+        self.assertTrue(pyohio.main().startswith('Hello'), "Starts with 'Hello'")
